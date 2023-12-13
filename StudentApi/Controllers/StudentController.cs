554f359f2ad6a48bcd81114c4385d23bce0c816b
thesn <thesn@BIGFROGGPC>
2023-12-13 20:58:12 -0300

    Adicionar arquivos de projeto.

---
+++ StudentApi/Controllers/StudentController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController(StudentContext context) : ControllerBase
+    {
+
+        // GET: api/Student
+        // Get a list of all students
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await context.Students.ToListAsync();
+        }
+
+        // GET: api/Student/5
+        // Get a student by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // PUT: api/Student/5
+        // Update a student by id
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                int v = await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Student
+        // Create a new student
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            context.Students.Add(student);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetStudent), new { id = student.Id }, student);
+        }
+
+        // DELETE: api/Student/5
+        // Delete a student by id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Student>> DeleteStudent(int id)
+        {
+            var student = await context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            context.Students.Remove(student);
+            await context.SaveChangesAsync();
+
+            return student;
+        }
+
+        // Private method to check if a student exists by id
+        private bool StudentExists(int id)
+        {
+            return context.Students.Any(e => e.Id == id);
+        }
+    }
+}
